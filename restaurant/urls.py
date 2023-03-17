b5921559c25e093b3937bf941b32606de521952c
Suzan Dewitz <suzanyaa@yahoo.com>
2023-03-17 05:53:23 +0000

    commit message

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls'), name='homepage_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
